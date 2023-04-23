018b4c9e50f0f0ba165c2d5cb68d08562638dd45
Diego Mauricio Pardo Calderón <128651145+DiegoPardoCalderon@users.noreply.github.com>
2023-04-23 17:24:00 -0400

    Desarrollo ejercicio en clases avance 1

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function ejercicio40(){
+    var nombre = prompt(`Ingrese el nombre`)
+    var apellido = prompt(`Ingrese el apellido`)
+    var edad = prompt(`Ingrese la edad`)
+    var estatura = prompt(`Ingrese la estatura en centimetros`)
+    var peso = prompt(`Ingrese el peso`)
+}
